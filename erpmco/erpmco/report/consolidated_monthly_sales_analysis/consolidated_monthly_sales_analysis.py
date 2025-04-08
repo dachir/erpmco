5a146f766efb3f13f71bff020d38ce048ab56190
dachir <dodziamouzou@gmail.com>
2025-04-08 18:22:18 +0000

    080420251300

--- erpmco/erpmco/report/consolidated_monthly_sales_analysis/consolidated_monthly_sales_analysis.py
+++ erpmco/erpmco/report/consolidated_monthly_sales_analysis/consolidated_monthly_sales_analysis.py
@@ -141,76 +141,76 @@ def get_data(filters=None):
 				INNER JOIN `tabUOM Conversion Detail` ucd ON ucd.parent = sii.item_code AND ucd.uom = sii.uom
 				WHERE
 					si.docstatus = 1  
-					AND si.posting_date BETWEEN %(month_start)s AND %(month_end)s  
+					AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s  
 					AND si.branch LIKE %(branch)s
 				GROUP BY sii.item_code, sii.item_name, si.branch
 			),
 			ranked_routing AS (
 				SELECT r.production_item, b.raw_material_cost, 
-						(b.scrap_material_cost * -1) AS scrap_material_credit, 
-						b.total_cost,
-						(w.hour_rate_electricity * o.time_in_mins / 60) AS factory_overhead, 
-						(w.hour_rate_consumable * o.time_in_mins / 60) AS other_overhead, 
-						(w.hour_rate_labour * o.time_in_mins / 60) AS labour,
-						(w.hour_rate_rent * o.time_in_mins / 60) AS depreciation,
-						b.total_cost + ((w.hour_rate_electricity + w.hour_rate_consumable + w.hour_rate_labour + w.hour_rate_rent) * o.time_in_mins / 60) AS std_cogs,
-						ROW_NUMBER() OVER (PARTITION BY r.production_item ORDER BY r.name) AS rn
+					(b.scrap_material_cost * -1) AS scrap_material_credit, 
+					b.total_cost,
+					(w.hour_rate_electricity * o.time_in_mins / 60) AS factory_overhead, 
+					(w.hour_rate_consumable * o.time_in_mins / 60) AS other_overhead, 
+					(w.hour_rate_labour * o.time_in_mins / 60) AS labour,
+					(w.hour_rate_rent * o.time_in_mins / 60) AS depreciation,
+					b.total_cost + ((w.hour_rate_electricity + w.hour_rate_consumable + w.hour_rate_labour + w.hour_rate_rent) * o.time_in_mins / 60) AS std_cogs,
+					ROW_NUMBER() OVER (PARTITION BY r.production_item ORDER BY r.name) AS rn
 				FROM `tabRouting` r 
 				INNER JOIN `tabBOM Operation` o ON o.parent = r.name 
 				INNER JOIN `tabWorkstation` w ON w.name = o.workstation
 				INNER JOIN `tabBOM` b ON b.item = r.production_item
 				WHERE b.docstatus = 1 AND b.is_active = 1 AND b.is_default = 1
 			),
-			cogs AS (
-				SELECT t.item_code, SUM(t.cogs) / SUM(t.stock_qty) AS cogs_rate_t
+			cogs AS(
+				SELECT t.item_code, SUM(t.cogs) / SUM(t.stock_qty) AS cogs_rate_t, SUM(free_qty) AS free_qty, SUM(cogs_free_qty) / SUM(t.free_qty) AS cogs_free_qty_t
 				FROM(
 					SELECT si.name, si.posting_date, si.customer, si.customer_name, dni.item_code, dni.item_name, dni.qty, 
 						CASE WHEN dni.stock_uom = 'T' THEN dni.stock_qty ELSE 0 END as stock_qty,
-						dni.stock_qty * dni.incoming_rate AS cogs, dni.parent, dni.against_sales_order
-					FROM `tabDelivery Note Item` dni 
-					INNER JOIN `tabSales Invoice Item` sii on dni.parent = sii.delivery_note and dni.item_code = sii.item_code
+						dni.stock_qty* dni.incoming_rate AS cogs, dni.parent, dni.against_sales_order,
+						CASE WHEN dni.is_free_item = 1 THEN dni.stock_qty ELSE 0 END AS free_qty,
+						CASE WHEN dni.is_free_item = 1 THEN dni.stock_qty* dni.incoming_rate ELSE 0 END AS cogs_free_qty
+					FROM `tabDelivery Note Item` dni INNER JOIN    
+						`tabSales Invoice Item` sii on dni.parent = sii.delivery_note and dni.item_code = sii.item_code
 					LEFT JOIN `tabSales Invoice` si ON si.name = sii.parent 
-					WHERE si.docstatus = 1 
-						AND si.posting_date BETWEEN %(month_start)s AND %(month_end)s 
-						AND si.branch LIKE %(branch)s
+					WHERE si.docstatus = 1 AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s AND si.branch LIKE %(branch)s
 					GROUP BY si.name, si.posting_date, si.customer, si.customer_name, dni.item_code, dni.item_name, 
-							dni.qty, dni.stock_qty, dni.amount, dni.parent, dni.against_sales_order
+						dni.qty, dni.stock_qty, dni.amount, dni.parent, dni.against_sales_order
 					ORDER BY si.posting_date, si.name
 				) AS t
 				GROUP BY t.item_code
 			)
 			SELECT v.*, v.std_net_sales_ct / weight_in_ct AS std_net_sales_t, v.std_net_sales_with_tax_ct / weight_in_ct AS std_net_sales_with_tax_t,
-					(v.std_net_sales_ct / weight_in_ct) - v.std_cogs AS gp, gross_amount / qty AS actual_cost_ct, gross_amount / stock_qty AS actual_cost_t,
-					CASE WHEN (v.std_net_sales_ct / weight_in_ct) <> 0 THEN 100 * (1 - v.std_cogs / (v.std_net_sales_ct / weight_in_ct)) ELSE 0 END AS gp_percent,
-					v.actual_gp / v.net_amount * 100 AS actual_gp_percent, v.actual_buying / v.stock_qty AS actual_cogs_t, 
-					v.factory_overhead + v.other_overhead + v.labour + v.depreciation AS conv_cost_t
+				(v.std_net_sales_ct / weight_in_ct) - v.std_cogs AS gp, gross_amount / qty AS actual_cost_ct, gross_amount / stock_qty AS actual_cost_t,
+				CASE WHEN (v.std_net_sales_ct / weight_in_ct) <> 0 THEN 100 * (1 - v.std_cogs / (v.std_net_sales_ct / weight_in_ct)) ELSE 0 END AS gp_percent,
+				v.actual_gp / v.net_amount * 100 AS actual_gp_percent, v.actual_buying / v.stock_qty AS actual_cogs_t, 
+				v.factory_overhead + v.other_overhead + v.labour + v.depreciation AS conv_cost_t
 			FROM(
-				SELECT t.*, t.price_list_rate - t.inv_disc - t.cash_disc - t.bonus - t.royalty AS std_net_sales_with_tax_ct, 
-						t.net_amount - t.actual_buying AS actual_gp,
-						(t.price_list_rate - t.inv_disc - t.cash_disc - t.bonus - t.royalty) * 0.16 / weight_in_ct AS std_tva,
-						(t.price_list_rate - t.inv_disc - t.cash_disc - t.bonus - t.royalty) * 0.84 * 0.1 * (IFNULL(dda,0) <> 0) / weight_in_ct AS std_dda,
-						(t.price_list_rate - t.inv_disc - t.cash_disc - t.bonus - t.royalty) * 0.84 * (1 - 0.1*(IFNULL(dda,0) <> 0)) * 0.0167 / weight_in_ct AS std_fpi,
-						(t.price_list_rate - t.inv_disc - t.cash_disc - t.bonus - t.royalty) * 0.84 * (1 - 0.1*(IFNULL(dda,0) <> 0)) * 0.9833 AS std_net_sales_ct
-				FROM (
-						SELECT s.*, r.*, ip.price_list, ip.price_list_rate, s.conversion_factor AS weight_in_ct, 
-								ip.price_list_rate / s.conversion_factor AS std_gross_rate, 
-								ip.price_list_rate * %(inv_disc_rate)s AS inv_disc,
-								ip.price_list_rate * %(csh_disc_rate)s * (1 - %(inv_disc_rate)s) AS cash_disc,
-								ip.price_list_rate * %(bonus_rate)s * (1 - %(inv_disc_rate)s) * (1 - %(csh_disc_rate)s) AS bonus,
-								CASE WHEN LOWER(s.item_name) LIKE '%%band%%' 
-									THEN ip.price_list_rate * %(royalty_rate)s * (1 - %(inv_disc_rate)s) * (1 - %(csh_disc_rate)s) 
-									ELSE 0 
-								END AS royalty, c.cogs_rate_t , c.cogs_rate_t * stock_qty AS actual_buying, c.free_qty, c.cogs_free_qty_t,
-								cat.description AS category, scat.description AS sub_category
-						FROM sales s 
-						INNER JOIN `tabItem Price` ip ON s.item_code = ip.item_code 
-						INNER JOIN tabItem i ON i.item_code = ip.item_code
-						INNER JOIN `tabFamille Statistique` cat ON cat.name = i.category
-						INNER JOIN `tabFamille Statistique` scat ON scat.name = i.sub_category
-						INNER JOIN ranked_routing r ON r.production_item = s.item_code AND r.rn = 1
-						LEFT JOIN cogs c ON c.item_code = s.item_code
-						WHERE LOWER(ip.price_list) LIKE CONCAT(LOWER(s.branch), ' gross', '%%') 
-							AND (%(month_start)s >= valid_from AND (%(month_start)s <= valid_upto OR valid_upto IS NULL))
+			SELECT t.*, t.price_list_rate - t.inv_disc - t.cash_disc - t.bonus - t.royalty AS std_net_sales_with_tax_ct, 
+				t.net_amount - t.actual_buying AS actual_gp,
+				(t.price_list_rate - t.inv_disc - t.cash_disc - t.bonus - t.royalty) * 0.16 / weight_in_ct AS std_tva,
+				(t.price_list_rate - t.inv_disc - t.cash_disc - t.bonus - t.royalty) * 0.84 * 0.1 * (IFNULL(dda,0) <> 0) / weight_in_ct AS std_dda,
+				(t.price_list_rate - t.inv_disc - t.cash_disc - t.bonus - t.royalty) * 0.84 * (1 - 0.1*(IFNULL(dda,0) <> 0)) * 0.0167 / weight_in_ct AS std_fpi,
+				(t.price_list_rate - t.inv_disc - t.cash_disc - t.bonus - t.royalty) * 0.84 * (1 - 0.1*(IFNULL(dda,0) <> 0)) * 0.9833 AS std_net_sales_ct
+			FROM (
+					SELECT s.*, r.*, ip.price_list, ip.price_list_rate, s.conversion_factor AS weight_in_ct, 
+						ip.price_list_rate / s.conversion_factor AS std_gross_rate, 
+						ip.price_list_rate * %(inv_disc_rate)s AS inv_disc,
+						ip.price_list_rate * %(csh_disc_rate)s * (1 - %(inv_disc_rate)s) AS cash_disc,
+						ip.price_list_rate * %(bonus_rate)s * (1 - %(inv_disc_rate)s) * (1 - %(csh_disc_rate)s) AS bonus,
+						CASE WHEN LOWER(s.item_name) LIKE '%%band%%' 
+								THEN ip.price_list_rate * %(royalty_rate)s * (1 - %(inv_disc_rate)s) * (1 - %(csh_disc_rate)s) 
+								ELSE 0 
+						END AS royalty, c.cogs_rate_t , c.cogs_rate_t * stock_qty AS actual_buying, c.free_qty, c.cogs_free_qty_t,
+						cat.description AS category, scat.description AS sub_category
+					FROM sales s 
+					INNER JOIN `tabItem Price` ip ON s.item_code = ip.item_code 
+					INNER JOIN tabItem i ON i.item_code = ip.item_code
+					INNER JOIN `tabFamille Statistique` cat ON cat.name = i.category
+					INNER JOIN `tabFamille Statistique` scat ON scat.name = i.sub_category
+					INNER JOIN ranked_routing r ON r.production_item = s.item_code AND r.rn = 1
+					LEFT JOIN cogs c ON c.item_code = s.item_code
+					WHERE LOWER(ip.price_list) LIKE CONCAT(LOWER(s.branch), ' gross', '%%') 
+					AND (%(from_date)s >= valid_from AND (%(from_date)s <= valid_upto OR valid_upto IS NULL))
 				) AS t
 			) AS v
 		"""
